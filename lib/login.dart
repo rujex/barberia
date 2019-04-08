3e62dbbf7c368cc48f4b6ec6fef07e30571553c3
Rubén <rujex93rujex@gmail.com>
2019-04-08 19:24:50 +0200

    login

---
+++ lib/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Iniciar sesion'),
+      ),
+      body: Form(
+          key: ,
+          child: Column(
+            children: <Widget>[
+
+            ],
+          )
+      ),
+    )
+  }
+}
